f00cd94069724661c36d490d8282990bc37fb341
ThinkPad <tyrontundrom@gmail.com>
2021-10-12 12:23:04 +0200

    Initial commit

---
+++ src/main/java/com/plyszkowski/assessment/PlyszkowskiApplication.java
@@ -0,0 +1,13 @@
+package com.plyszkowski.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlyszkowskiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlyszkowskiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plyszkowski/assessment/PlyszkowskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plyszkowski.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlyszkowskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
