e6e820caa7ab416928925d107d869392ee3aede8
ThinkPad <tyrontundrom@gmail.com>
2021-10-16 20:51:37 +0200

    add EmployeeService

---
+++ src/main/java/com/plyszkowski/assessment/repository/EmployeesRepository.java
@@ -0,0 +1,9 @@
+package com.plyszkowski.assessment.repository;
+
+import com.plyszkowski.assessment.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeesRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/plyszkowski/assessment/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package com.plyszkowski.assessment.service;
+
+import com.plyszkowski.assessment.model.Employee;
+import com.plyszkowski.assessment.repository.EmployeesRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeesRepository employeesRepository;
+
+    public EmployeeService(EmployeesRepository employeesRepository) {
+        this.employeesRepository = employeesRepository;
+    }
+
+    public Employee create(Employee employee) {
+        return employeesRepository.save(employee);
+    }
+
+    public List<Employee> findAll() {
+        return employeesRepository.findAll();
+    }
+
+    public Optional<Employee> findOneEmploye(Example<Employee> employeeExample) {
+        return employeesRepository.findOne(employeeExample);
+    }
+
+    public void delete(Employee employee) {
+        employeesRepository.delete(employee);
+    }
+
+
+
+
+}
